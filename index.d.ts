29b31b3dbbb0e6404f696c870f4aac7d469019e6
Aravindan Ve <aravindan.ve@outlook.com>
2020-07-21 22:48:09 +0530

    add typescript type definitions

---
+++ index.d.ts
@@ -0,0 +1,63 @@
+import * as Koa from 'koa';
+
+declare namespace Router {
+  export type RouteHandle = Middleware | RouteFragment | Router;
+  export type RouteLookup = Middleware | undefined;
+  export type RouteParams = Record<string, string | undefined>;
+
+  export type Context = Koa.ParameterizedContext<
+    Koa.DefaultState,
+    RouterContext & Koa.DefaultContext
+  >;
+
+  export type Middleware = Koa.Middleware<
+    Koa.DefaultState,
+    RouterContext & Koa.DefaultContext
+  >;
+
+  export type RoutingOptions = {
+    prefix?: string;
+    caseSensitive?: boolean;
+    strict?: boolean;
+  };
+
+  export type RouterContext = {
+    params: RouteParams;
+  };
+
+  export interface RegisterHandle {
+    (...middleware: RouteHandle[]): Router;
+    (path: string, ...middlewares: RouteHandle[]): Router;
+  }
+
+  export { RouteFragment as Fragment };
+}
+
+declare class RouteFragment {
+  readonly type: number;
+  readonly chain: unknown[];
+  readonly prefix: string;
+  readonly caseSensitive: boolean;
+  readonly strict: boolean;
+  constructor(options?: Router.RoutingOptions);
+  add(method: string, ...args: Parameters<Router.RegisterHandle>): this;
+  use: Router.RegisterHandle;
+  all: Router.RegisterHandle;
+  get: Router.RegisterHandle;
+  post: Router.RegisterHandle;
+  put: Router.RegisterHandle;
+  delete: Router.RegisterHandle;
+  del: Router.RegisterHandle;
+}
+
+declare class Router extends RouteFragment {
+  lookup(
+    method: string,
+    path: string,
+    params?: Router.RouteParams,
+  ): Router.RouteLookup;
+  handle(ctx: Koa.ParameterizedContext, next: Koa.Next): ReturnType<Koa.Next>;
+  routes(): Router.Middleware;
+}
+
+export = Router;
---
+++ test/types.ts
@@ -0,0 +1,60 @@
+import * as Koa from 'koa';
+import * as Router from '..';
+
+const app = new Koa();
+const noop = () => {};
+const router = new Router({
+  prefix: '',
+  caseSensitive: false,
+  strict: true,
+});
+
+router.use((ctx) => {
+  ctx.params.name; // string | undefined
+});
+
+router
+  .use(noop)
+  .use(new Router.Fragment().use(noop))
+  .use(
+    new Router.Fragment({
+      prefix: '/test',
+      caseSensitive: false,
+      strict: true,
+    }).use(noop),
+  )
+  .use(new Router().use(noop))
+  .use(
+    new Router()
+      .use(noop)
+      .use('/path', noop)
+      .use(noop, noop)
+      .use('/path', noop, noop)
+      .get(noop)
+      .get('/path', noop)
+      .get(noop, noop)
+      .get('/path', noop, noop)
+      .post(noop)
+      .post('/path', noop)
+      .post(noop, noop)
+      .post('/path', noop, noop)
+      .put(noop)
+      .put('/path', noop)
+      .put(noop, noop)
+      .put('/path', noop, noop)
+      .delete(noop)
+      .delete('/path', noop)
+      .delete(noop, noop)
+      .delete('/path', noop, noop)
+      .del(noop)
+      .del('/path', noop)
+      .del(noop, noop)
+      .del('/path', noop, noop)
+      .all(noop)
+      .all('/path', noop)
+      .all(noop, noop)
+      .all('/path', noop, noop),
+  );
+
+app.use(router.routes());
+app.listen();
